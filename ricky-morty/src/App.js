963e694af26718ce12e03a6e91b16f247a305620
gustavo <go8895806@gmail.com>
2021-09-06 17:02:24 -0300

    adicionando novas paginas

--- ricky-morty/src/App.js
+++ ricky-morty/src/App.js
@@ -1,22 +1,43 @@
 import './App.css';
 import Character from './components/Characters';
-
+import Message from './components/Message';
 import { useState, useEffect } from 'react';
 function App() {
   const [ characterList, setcharacterList ] = useState(null)
+  const [page, setPage] = useState(1)
+  const [watchingPages, setWatchingPages  ] = useState(0)
   useEffect(()=>{
-    fetch('https://rickandmortyapi.com/api/character/')
+    fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
     .then(response =>{
       return response.json()
     })
     .then((data) =>{
       setcharacterList(data)
     })
-  },[])
-  console.log(characterList)
+  },[page])
+  const nextPage = () =>{
+    setPage(page + 1)
+    setWatchingPages(0)
+    
+  }
+  const previousPage = () =>{
+    if(page > 1){
+      setPage(page-1)
+    }
+    if(page===1){
+      setWatchingPages(1)
+    }
+    
+  }
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Meus personagens</h1>
+        <section>
+          <button onClick={previousPage} className='nav-controll-buttons'>Previous</button>
+          <button  onClick = {nextPage} className='nav-controll-buttons'>Next</button>
+        </section>
+        { watchingPages=== 1  &&  <Message></Message>}
         {characterList && <Character charactersList={characterList}></Character>}
       </header>
     </div>
---
+++ ricky-morty/src/components/Message/index.jsx
@@ -0,0 +1,8 @@
+import './style.css'
+const Message = () =>{
+    return(
+        <p className='error-msg'>Impossível verificar a pagina anterior </p>
+
+    )
+}
+export default Message
